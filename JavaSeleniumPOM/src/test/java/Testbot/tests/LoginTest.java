595115163a30b1f952e4705ea129dd21ee0bd8bb
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-10-21 19:21:56 +0530

    changed to utilize InitTest class

--- JavaSeleniumPOM/src/test/java/Testbot/tests/LoginTest.java
+++ JavaSeleniumPOM/src/test/java/Testbot/tests/LoginTest.java
@@ -1,34 +1,14 @@
-package tests;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeTest;
+package Testbot.tests;
 import org.testng.annotations.Test;
-import org.testng.annotations.AfterTest;
-// Import all required pages for Login
-import pages.Login;
-
+import Testbot.pages.Login;
 public class LoginTest{
-    WebDriver driver;
     Login flipkartSignIn;
-    @BeforeTest
-    public void setup(){
-        System.setProperty("webdriver.gecko.driver","/home/thebadcoder/TestWorkspace/drivers/geckodriver");
-        driver = new FirefoxDriver();
-        String baseURL = "https://www.flipkart.com";
-        driver.get(baseURL);
-        driver.manage().window().maximize();
-    }
     
     @Test(priority=0)
     public void credentialsTest(){
-        flipkartSignIn = new Login(driver);
-        flipkartSignIn.fillUsername("dummyuser");
-        flipkartSignIn.fillPassword("dummypassword");
+        flipkartSignIn = new Login();
+        flipkartSignIn.fillUsername("RealUserName");
+        flipkartSignIn.fillPassword("RealUserPassword");
         flipkartSignIn.clickLogin();
     } 
-
-    @AfterTest
-    public void tearDown(){
-        driver.close();
-    }
 }
